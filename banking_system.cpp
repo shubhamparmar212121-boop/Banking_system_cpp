cd50a44ab548442214debd883dbdfa45ed42bc80
shubhamparmar212121-boop <shubhamparmar212121@gmail.com>
2026-03-05 14:42:47 +0530

    make function

--- banking_system.cpp
+++ banking_system.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
-using namespace std;
+#include<string>
 
+
+using namespace std;
+#define MIN_BALANCE 500;
+
+//CLASS ACCOUNT
+class Account{
+    private:
+    long AccountNumber;
+    string FirstName;
+    string LastName;
+    float balance;
+    static long NextAccountNumber;
+    public:
+    Account(){}
+    Account(string fname,string lname,float balance);
+    long getAccNo(){return AccountNumber;}
+    string getFirstname(){return FirstName;}
+    string getLastName(){return LastName;}
+    float getBalance(){return balance;}
+    void diposit(float mount);
+    void withdraw(float amount);
+    static void setlastaccountnumber(long accountnumber);
+    static long setlastaccountnumber();
+
+};
 
 
 
